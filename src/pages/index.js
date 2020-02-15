8f1d474d5e7528df380cc3fed181c3e56252c719
k_yagi <k.yagi914@gmail.com>
2020-02-15 19:04:59 +0900

    apiで取得したjsonから動画id取り出してプレイヤーで表示するまで

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,78 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = props.data;
+  }
 
-export default IndexPage
+  componentDidMount() {
+    const self = this;
+    const tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    let player = undefined;
+    window.onYouTubeIframeAPIReady = function () {
+      player = new window.YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: self.state.allContentsJson.edges[0].node.items[0].id.videoId,
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+
+    function onPlayerReady(event) {
+      event.target.playVideo();
+    }
+
+    let done = false;
+    function onPlayerStateChange(event) {
+      if (event.data === window.YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+      }
+    }
+    function stopVideo() {
+      player.stopVideo();
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div id="player"></div>
+      </Layout>
+    )
+  }
+}
+
+export default IndexComponent
+
+export const query = graphql`
+  query {
+    allContentsJson {
+      edges {
+        node {
+          items {
+            id {
+              videoId
+            }
+            snippet {
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  }
+`
