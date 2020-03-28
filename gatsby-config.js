468146c8070d0b2a79c0629dfeb279294e13c2d9
k_yagi <k.yagi914@gmail.com>
2020-03-28 15:52:08 +0900

    siteMetadata修正、material-ui設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Youtube Live Viewer`,
+    description: `Youtube Live Viewer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -34,6 +33,7 @@ module.exports = {
         path: `./src/contents/`,
       }
     },
+    `gatsby-theme-material-ui`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
