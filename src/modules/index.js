74aaa3de80879135bbd170b830690601ae32e11f
k_yagi <k.yagi914@gmail.com>
2020-05-16 17:40:37 +0900

    videos読み込み

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,6 @@
 // 各moduleはexport defaultでreducerを渡すようにして、ここでcombineReducerでまとめる感じか？
 import { combineReducers } from 'redux';
 import contents from "./contents";
+import videos from "./videos";
 
-export default combineReducers({ contents });
+export default combineReducers({ contents, videos });
