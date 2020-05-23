334f695ecb54f9893371fd75612183160725602b
k_yagi <k.yagi914@gmail.com>
2020-05-23 17:30:11 +0900

    開発/本番時で処理分岐

--- src/store.js
+++ src/store.js
@@ -1,4 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./modules/index";
 
-export default () => createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let storeCreated = null
+
+if (typeof window !== 'undefined') {
+  // 開発時
+  storeCreated = () => createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+} else {
+  // 本番ビルド時
+  storeCreated = () => createStore(rootReducer)
+}
+
+export default storeCreated
+
